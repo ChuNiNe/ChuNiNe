3fe97addeb846acc566d869df66662c9dd9e97d0
chuunine <yie.1325@163.com>
2021-02-17 16:01:53 +0800

    label

---
+++ HelloWorld1/HelloWorld1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HelloWorld1
+//
+//  Created by make on 2021/2/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HelloWorld1/HelloWorld1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HelloWorld1
+//
+//  Created by make on 2021/2/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
